425cf638cedf99ed5d22f0f17856e3342e59c47d
Belete-Dereje <b30613155@gmail.com>
2026-03-21 20:16:42 +0300

    hi

--- arr.php
+++ arr.php
@@ -9,4 +9,9 @@
   foreach($person as $name=>$age){
     echo "$name is $age years old<br>";
   }
+
+  $age = array("Peter"=>35, "Ben"=>37, "Joe"=>43);
+  echo "Peter is " . $age['Peter'] . " years old.";
+  echo "Ben is " . $age['Ben']. " years old.";
+  echo "Joe is " . $age['Joe‟] . " years old.";
 ?>
