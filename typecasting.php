2f9342a411a3cf446dfe6f8c11b51e656088090b
Belete-Dereje <b30613155@gmail.com>
2026-03-21 19:18:09 +0300

    hello update

--- typecasting.php
+++ typecasting.php
@@ -1,5 +1,5 @@
 <?php
-
+//TEMPORARY
 // Original values
 $numberString = "25";
 $floatNumber = 10.75;
@@ -29,5 +29,38 @@ echo "Float to String: " . $stringValue . "<br>";
 // 6. Cast string with text to integer
 $intFromText = (int) $text;
 echo "Text to Integer: " . $intFromText . "<br>";
+
+
+//PERMANENT
+// Original variable
+$value = "50";
+
+echo "Original value: " . $value . "<br>";
+echo "Original type: " . gettype($value) . "<br><br>";
+
+// Convert to integer permanently
+settype($value, "integer");
+
+echo "After settype to integer: " . $value . "<br>";
+echo "New type: " . gettype($value) . "<br><br>";
+
+// Convert to float
+settype($value, "float");
+
+echo "After settype to float: " . $value . "<br>";
+echo "New type: " . gettype($value) . "<br><br>";
+
+// Convert to string
+settype($value, "string");
+
+echo "After settype to string: " . $value . "<br>";
+echo "New type: " . gettype($value) . "<br><br>";
+
+// Convert to boolean
+settype($value, "boolean");
+
+echo "After settype to boolean: " . $value . "<br>";
+echo "New type: " . gettype($value) . "<br>";
+
 
 ?>
